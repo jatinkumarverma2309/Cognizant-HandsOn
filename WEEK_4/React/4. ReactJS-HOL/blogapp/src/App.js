3d61b7a1109b28ef2caea650299c7fe9157d17fa
jatinkumarverma2309 <rajputjatin614@gmail.com>
2026-07-10 01:24:05 +0530

    React

---
+++ WEEK_4/React/4. ReactJS-HOL/blogapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Posts from './Post';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Posts App</h1>
+      <Posts />
+    </div>
+  );
+}
+
+export default App;
